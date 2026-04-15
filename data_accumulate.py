b18431b598954d7c2e5375228a5b9ae7cca3bf14
yintao1834 <wangyintao180018@gmail.com>
2026-04-15 20:13:39 +0900

    first commit

---
+++ data_accumulate.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import charset_normalizer
+
+# 
+BASE_DIR = Path(__file__).parent
+data_path = BASE_DIR/"data"/"case1"/"2(5.0-5.0)_strain.csv"
+with open(data_path,'rb') as f:
+    rawdata = f.read()
+result = charset_normalizer.detect(rawdata)
+df = pd.read_csv(data_path, encoding = result['encoding'], usecols=['Distance (m)','Time 1'], 
+                 index_col=['Distance (m)'])
+df.columns = ['strain']
+print(df)
